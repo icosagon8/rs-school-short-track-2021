131bf76fe7ba2ee3b23cf6631230c81cd152c120
icosagon8 <dmpavlov8@yandex.ru>
2021-04-09 18:37:50 +0300

    feat: add solution for 05-delete-digit task

--- src/05-delete-digit.js
+++ src/05-delete-digit.js
@@ -9,8 +9,17 @@
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new Error('Not implemented');
+function deleteDigit(n) {
+  const str = n.toString();
+  const arr = [];
+
+  for (let i = 0; i < str.length; i++) {
+    arr.push(+(str.slice(0, i) + str.slice(i + 1)));
+  }
+
+  const maxNumber = Math.max(...arr);
+
+  return maxNumber;
 }
 
 module.exports = deleteDigit;
