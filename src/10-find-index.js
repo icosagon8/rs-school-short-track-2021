b538fdf2ed0492cbbb1235a8a496cef0f82e5ce0
icosagon8 <dmpavlov8@yandex.ru>
2021-04-10 20:03:21 +0300

    feat: add solution for 10-find-index task

--- src/10-find-index.js
+++ src/10-find-index.js
@@ -11,8 +11,24 @@
  * For ([1, 2, 3], 2) should return 1
  *
  */
-function findIndex(/* array, value */) {
-  throw new Error('Not implemented');
+function findIndex(array, value) {
+  let lowIndex = 0;
+  let highIndex = array.length - 1;
+
+  while (lowIndex <= highIndex) {
+    const averageIndex = Math.floor((lowIndex + highIndex) / 2);
+    const currentValue = array[averageIndex];
+
+    if (currentValue === value) {
+      return averageIndex;
+    } if (currentValue > value) {
+      highIndex = averageIndex - 1;
+    } else {
+      lowIndex = averageIndex + 1;
+    }
+  }
+
+  return 'There is no such value in the array';
 }
 
 module.exports = findIndex;
