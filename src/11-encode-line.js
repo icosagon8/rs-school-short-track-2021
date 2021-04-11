812d4863f91b04aa3c72524b6f78b9c196e86076
icosagon8 <dmpavlov8@yandex.ru>
2021-04-11 15:23:07 +0300

    feat: add solution for 11-encode-line task

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,23 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  let count = 1;
+  let result = '';
+  const lastChar = str[str.length - 1] || '';
+
+  for (let i = 1; i < str.length; i++) {
+    if (str[i] === str[i - 1]) {
+      count++;
+    } else {
+      result += (count > 1) ? `${count}${str[i - 1]}` : str[i - 1];
+      count = 1;
+    }
+  }
+
+  result += ((count === 1) ? '' : count) + lastChar;
+
+  return result;
 }
 
 module.exports = encodeLine;
