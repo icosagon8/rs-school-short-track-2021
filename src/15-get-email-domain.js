110971630e0b66fe306ee684277f30ba19b798d2
icosagon8 <dmpavlov8@yandex.ru>
2021-04-11 18:44:29 +0300

    feat: add solution for 15-get-email-domain task

--- src/15-get-email-domain.js
+++ src/15-get-email-domain.js
@@ -8,8 +8,11 @@
  * For the input 'prettyandsimple@example.com', the output should be 'example.com'
  *
  */
-function getEmailDomain(/* email */) {
-  throw new Error('Not implemented');
+function getEmailDomain(email) {
+  const regex = /@[a-z\d-]+\.[a-z]+$/gm;
+  const emailDomain = email.match(regex)[0].slice(1);
+
+  return emailDomain;
 }
 
 module.exports = getEmailDomain;
